492cccd02b8a7c540b385e723b4f5a882dc49cca
notreallyaclue <38693819+notreallyaclue@users.noreply.github.com>
2022-01-12 19:17:11 +0000

    Add files via upload

---
+++ Gazebo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+
+gazebo --verbose worlds/multi_drone5.world
+
+
---
+++ SITL.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+
+~/ardupilot/Tools/autotest/sim_vehicle.py -f gazebo-iris --console -v ArduCopter -n5 --auto-sysid -w -m "--show-errors"
